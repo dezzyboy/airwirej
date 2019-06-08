8bbd4791b8fb59271c6fb6c873289e899dc1307f
Akshay <akshaycm@hotmail.com>
2019-06-08 16:47:41 +0300

    update Protocol ver

--- core/src/main/java/org/airwirej/core/CoinDefinition.java
+++ core/src/main/java/org/airwirej/core/CoinDefinition.java
@@ -79,8 +79,8 @@ public class CoinDefinition {
     //
     // WIRE 0.12
     //
-    public static final int PROTOCOL_VERSION = 70020;          //version.h PROTOCOL_VERSION
-    public static final int MIN_PROTOCOL_VERSION = 70002;        //version.h MIN_PROTO_VERSION
+    public static final int PROTOCOL_VERSION = 70021;          //version.h PROTOCOL_VERSION
+    public static final int MIN_PROTOCOL_VERSION = 70021;        //version.h MIN_PROTO_VERSION
 
     public static final int BLOCK_CURRENTVERSION = 2;   //CBlock::CURRENT_VERSION
     public static final int MAX_BLOCK_SIZE = 1 * 1000 * 1000;
